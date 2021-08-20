d5e94781817b9a98a7dc39c86e635d42b702e945
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-08-20 12:20:42 -0500

    First Commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+export const AppRouter = () => {
+    return (
+        
+    )
+}
